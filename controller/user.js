9ff246b19e988c130704b55f7859e2a55a836a7e
Nitesh Kumar <62844049+Niteshthori24198@users.noreply.github.com>
2023-07-02 14:13:41 +0530

    Add files via upload

---
+++ controller/user.js
@@ -0,0 +1,40 @@
+
+const usersDB = [];
+const userJoined = (obj) =>{
+    usersDB.push(obj);
+}
+const leaveRoom = (sid) => {
+    const ind = usersDB.findIndex((ele)=>ele.id===sid);
+    
+    if(ind != -1){
+        const deleteduser =  usersDB.splice(ind, 1)[0]
+        return deleteduser
+    }
+
+}
+const getUser = (sid) => {
+    const user = usersDB.find((ele)=> ele.id===sid)
+    return user
+}
+
+const getUsersList = (room) => {
+
+    const users = usersDB.reduce((acc,curr)=>{
+        if(curr.room===room){
+            acc.push(curr.username)
+        }
+        return acc
+    },[])
+
+    return {
+        room:room,
+        users: users
+    }
+}
+
+module.exports = {
+    userJoined,
+    leaveRoom,
+    getUser,
+    getUsersList
+}
---
+++ server.js
@@ -0,0 +1,53 @@
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const {Server} = require('socket.io');
+
+const { userJoined, leaveRoom, getUser, getUsersList } = require('./controller/user');
+const { formateMsg } = require('./controller/message');
+
+const io = new Server(server);
+const chatNSP = io.of('/chat');
+
+chatNSP.on("connection", (socket) => {
+    console.log('user is joined');
+    socket.on('joinRoom', ({username,room})=>{
+
+        userJoined({username, room, id:socket.id});
+
+        socket.join(room);
+
+        socket.emit("message", formateMsg("Server", "Thank you for joinning"));
+
+        socket.broadcast.to(room).emit("message", formateMsg("Server", `${username} has joined the chat`));
+
+        chatNSP.to(room).emit("userlist", getUsersList(room))
+       
+    })
+
+
+
+
+    socket.on('sendMsg', (msg)=>{
+      
+        const user = getUser(socket.id);
+        chatNSP.to(user.room).emit("message", formateMsg(user.username, msg));
+    })
+
+    socket.on('disconnect', ()=>{
+        
+        const user = leaveRoom(socket.id)
+        
+        console.log('user leave',user.username);
+        chatNSP.to(user.room).emit("userlist", getUsersList(user.room))
+
+        socket.broadcast.to(user.room).emit("message", formateMsg("Server", `${user.username} has left the chat`));
+    })
+})
+
+
+server.listen(3000,()=>{
+    console.log(`server is running on port 3000.`);
+})
